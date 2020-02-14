1406fd80a36dd9687bff86eea8e37a01e637a055
ubuvolt <ubuvolt@gmail.com>
2020-02-14 16:26:14 +0100

    {add re-define signature functionality}

--- app/CentralSetting.php
+++ app/CentralSetting.php
@@ -6,7 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class CentralSetting extends Model {
 
-    const E_SIGNATURE = 'fire-stopping-installation-signature';
+    const E_SIGNATURE = 'change-style-signature-in';
+    const IN_PROCESS = 'in-process';
+    const RE_E_SIGNATURE = 'change-style-signature-re-in';
+    const CONTACT = 'You can not change you signature any more, please contact with admin';
 
     protected $table = 'centralSetting';
 
